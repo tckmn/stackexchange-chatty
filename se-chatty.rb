b1b85734991c5d699c95d5eafc1b72368778c0a5
KeyboardFire <oddllamaprod@gmail.com>
2014-04-17 19:28:47 -0500

    added lots of features

--- se-chatty.rb
+++ se-chatty.rb
@@ -24,6 +24,7 @@ class SEChatty
 		@agent = agent
 		@sitename = sitename
 		@default_room_number = default_room_number
+		@previous_message = ''
 	end
 
 	def send_message room_number, message = nil
@@ -31,7 +32,20 @@ class SEChatty
 			message = room_number
 			room_number = @default_room_number
 		end
-		@agent.post("http://chat.#{@sitename}/chats/#{room_number}/messages/new", [['text', message], ['fkey', @fkey]])
+		message += '.' if message == @previous_message
+		loop {
+			success = false
+			begin
+				resp = @agent.post("http://chat.#{@sitename}/chats/#{room_number}/messages/new", [['text', message.slice(0, 500)], ['fkey', @fkey]]).body
+				success = JSON.parse(resp)['id'] != nil
+			rescue Mechanize::ResponseCodeError => e
+				puts "Error: #{e.inspect}"
+			end
+			break if success
+			puts 'sleeping'
+			sleep 3
+		}
+		@previous_message = message
 	end
 
 	def get_messages room_number = @default_room_number
@@ -47,12 +61,60 @@ class SEChatty
 			end
 		}
 	end
+
+	module Event
+		MessagePosted = 1
+		MessageEdited = 2
+		UserEntered = 3
+		UserLeft = 4
+		RoomNameChanged = 5
+		MessageStarred = 6
+		DebugMessage = 7
+		UserMentioned = 8
+		MessageFlagged = 9
+		MessageDeleted = 10
+		FileAdded = 11
+		ModeratorFlag = 12
+		UserSettingsChanged = 13
+		GlobalNotification = 14
+		AccessLevelChanged = 15
+		UserNotification = 16
+		Invitation = 17
+		MessageReply = 18
+		MessageMovedOut = 19
+		MessageMovedIn = 20
+		TimeBreak = 21
+		FeedTicker = 22
+		UserSuspended = 29
+		UserMerged = 30
+	end
 end
 
-# for this example, I'm using http://chat.stackexchange.com/rooms/11540/charcoal-hq
-sec = SEChatty.new 'stackexchange.com', 'INSERT EMAIL HERE', 'INSERT PASSWORD HERE', 11540
-# sec.send_message "Example message"
+# for this example, I'm using http://chat.stackexchange.com/rooms/13972/bot-testing-room
+sec = SEChatty.new 'stackexchange.com', 'INSERT EMAIL HERE', 'INSERT PASSWORD HERE', 13972
+sec.send_message 13972, 'Bot initialized'
 sec.get_messages {|event|
-  # this is called when a message arrives (whether it be a user entering the room, an edit, a star, etc.)
-	p event.data
+	JSON.parse(event.data).each do |room, data|
+		room_number = room.match(/\d+/)[0]
+		if data['e']
+			data['e'].each do |e|
+				p e
+				next if e['user_id'] == 110309 # my chatbot's id
+				case e['event_type']
+				when SEChatty::Event::MessagePosted, SEChatty::Event::MessageEdited
+					sec.send_message ":#{e['message_id']} Hello! That message was #{e['content']} and your username is #{e['user_name']}." if e['content'] =~ /hello/i
+				when SEChatty::Event::UserEntered
+					sec.send_message "Hi #{e['user_name']}!"
+				when SEChatty::Event::UserLeft
+					sec.send_message "Bye #{e['user_name']}!"
+				when SEChatty::Event::RoomNameChanged
+					sec.send_message "I #{e['content'].sum % 2 == 0 ? "don't" : ''}like that new name."
+				when SEChatty::Event::MessageStarred
+					sec.send_message "#{e['user_name']} starred that."
+				when SEChatty::Event::MessageDeleted
+					sec.send_message ":#{e['message_id']} (removed) to you too."
+				end
+			end
+		end
+	end
 }
