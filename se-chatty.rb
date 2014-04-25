6270d919782596b928956ab0a45084a2cbe28440
KeyboardFire <oddllamaprod@gmail.com>
2014-04-24 19:56:19 -0500

    my hands are typing words

--- se-chatty.rb
+++ se-chatty.rb
@@ -25,10 +25,24 @@ class SEChatty
 		@sitename = sitename
 		@default_room_number = default_room_number
 		@previous_message = ''
+
+		# start the rate-limit thread
+		@rate_limit = Thread.new {
+			loop {
+				sleep 7.5
+				@rate_limit_count = 0
+			}
+		}
+		@rate_limit_count = 0
 	end
 
 	def send_message message, room_number = @default_room_number
-		message += '.' if message == @previous_message
+		return if message == @previous_message # prevent duplicate messages
+
+		# rate limit ftw!
+		message = 'Rate limit reached.' if @rate_limit_count == 5
+		return if @rate_limit_count > 5
+
 		loop {
 			success = false
 			begin
@@ -42,19 +56,22 @@ class SEChatty
 			sleep 3
 		}
 		@previous_message = message
+		@rate_limit_count += 1
 	end
 
 	def get_messages room_number = @default_room_number
 		ws_url = JSON.parse(@agent.post("http://chat.#{@sitename}/ws-auth", [['roomid', room_number], ['fkey', @fkey]]).body)['url']
 		ws_url += '?l=' + JSON.parse(@agent.post("http://chat.#{@sitename}/chats/#{room_number}/events", [['fkey', @fkey]]).body)['time'].to_s
-		EM.run {
-			ws = Faye::WebSocket::Client.new(ws_url, nil, {
-				headers: {'Origin' => "http://chat.#{@sitename}"}
-			})
+		Thread.new {
+			EM.run {
+				ws = Faye::WebSocket::Client.new(ws_url, nil, {
+					headers: {'Origin' => "http://chat.#{@sitename}"}
+				})
 
-			ws.on :message do |event|
-				yield event
-			end
+				ws.on :message do |event|
+					yield event
+				end
+			}
 		}
 	end
 
@@ -88,7 +105,19 @@ end
 
 # for this example, I'm using http://chat.stackexchange.com/rooms/13972/bot-testing-room
 sec = SEChatty.new 'stackexchange.com', 'INSERT EMAIL HERE', 'INSERT PASSWORD HERE', 13972
-sec.send_message 'Bot initialized'
+prefix = '>>'
+sec.send_message "Bot initialized. Type #{prefix}help for help."
+cmds = {
+	help: ->a{
+		sec.send_message "Type #{prefix}listcommands to list all commands that the chatbot can execute."
+	},
+	listcommands: ->a{
+		sec.send_message "List of commands: #{cmds.keys * ', '}"
+	}
+}
+unknown_cmd = ->cmdname, a{
+	sec.send_message "Unknown command #{cmdname}."
+}
 sec.get_messages {|event|
 	JSON.parse(event.data).each do |room, data|
 		room_number = room.match(/\d+/)[0]
@@ -98,20 +127,53 @@ sec.get_messages {|event|
 				next if e['user_id'] == 110309 # my chatbot's id
 				case e['event_type']
 				when SEChatty::Event::MessagePosted, SEChatty::Event::MessageEdited
-					sec.send_message ":#{e['message_id']} Hello! That message was \"#{e['content']}\" and your username is #{e['user_name']}." if e['content'] =~ /hello/i
+					e['content'] = CGI.unescapeHTML e['content']
+					if e['content'].start_with? prefix
+						cmd = e['content'].sub prefix, ''
+						cmdname, args = (cmd.empty? ? '' : cmd.split(' ', 2))
+						cmdfunc = cmds[cmdname.to_sym]
+						cmdfunc ? cmdfunc[args] : unknown_cmd[cmdname, args]
+					end
 				when SEChatty::Event::UserEntered
-					sec.send_message "Hi #{e['user_name']}!"
+					#sec.send_message "Hi #{e['user_name']}!"
 				when SEChatty::Event::UserLeft
-					sec.send_message "Bye #{e['user_name']}!"
+					#sec.send_message "Bye #{e['user_name']}!"
 				when SEChatty::Event::RoomNameChanged
 					sec.send_message "I #{e['content'].sum % 2 == 0 ? "don't " : ''}like that new name and description."
 				when SEChatty::Event::MessageStarred
 					sec.send_message "#{e['user_name']} starred that."
 				when SEChatty::Event::MessageDeleted
-					sec.send_message ":#{e['message_id']} (removed) to you too."
+					sec.send_message "(removed) to you too, #{e['user_name']}."
 				end
 			end
 		end
 	end
 }
-at_exit { sec.send_message "I've been killed!" }
+Signal.trap('SIGINT') {
+	sec.send_message 'Bot killed manually'
+}
+conditions = [
+	{
+		cond: ->data{ data['title'] == data['title'].upcase },
+		msg: 'Allcaps title'
+	},
+	{
+		cond: ->data{ data['title'] =~ /\b(2014|baba)\b/i },
+		msg: 'BAD KEYWORD - SPAM @Doorknob @hichris123'
+	}
+]
+EM.run {
+	ws = Faye::WebSocket::Client.new('ws://qa.sockets.stackexchange.com/')
+
+	ws.on :open do |event|
+		ws.send '155-questions-active'
+	end
+
+	ws.on :message do |event|
+		data = JSON.parse JSON.parse(event.data)['data']
+		data['title'] = data['titleEncodedFancy']
+		data['site'] = data['apiSiteParameter']
+		cond = conditions.find{|cond| cond[:cond][data] }
+		sec.send_message "#{cond[:msg]} on #{data['site']}: [#{data['title']}](#{data['url']})" if cond
+	end
+}
